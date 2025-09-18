1d9463d2264fa3698f908690a398e8cf8bdaca13
dettone <dettonex25@gmail.com>
2025-09-18 18:21:38 -0300

    feat: database and .env

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,5 @@
 test("GET to api/status returns 200 and correct message", async () => {
-  const response = await fetch("http://localhost:3000/api/status");
+  const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
   const data = await response.json();
   expect(data.message).toBe("tabnews online!");
