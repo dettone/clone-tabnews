d6596757a549f3d8b43f095ec1a994f458265f2d
dettone <dettonex25@gmail.com>
2025-09-22 20:09:02 -0300

    feat: atualizando mais testes

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,22 @@
 import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result);
-  return response.status(200).send({
-    message: "tabnews online!",
+  const databaseName = process.env.POSTGRES_DB;
+  const version = await database.query("SHOW server_version;");
+
+  const maxConnections = await database.query("SHOW max_connections");
+  const updateAt = new Date().toISOString();
+
+  const usedConnections = await database.query({
+    text: `SELECT count(*) AS used_connections FROM pg_stat_activity WHERE datname = $1;`,
+    values: [databaseName],
+  });
+  console.log(usedConnections.rows);
+  response.status(200).json({
+    updated_at: updateAt,
+    version: version.rows[0].server_version,
+    maxConnections: Number(maxConnections.rows[0].max_connections),
+    usedConnections: Number(usedConnections.rows[0].used_connections),
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,6 +1,24 @@
 test("GET to api/status returns 200 and correct message", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
-  const data = await response.json();
-  expect(data.message).toBe("tabnews online!");
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+
+  expect(responseBody.updated_at).toBe(parsedUpdatedAt);
+
+  expect(responseBody.version).toBeDefined();
+  expect(responseBody.version).toEqual("16.0");
+  expect(responseBody.maxConnections).toBeDefined();
+  expect(typeof responseBody.maxConnections).toBe("number");
+  expect(responseBody.maxConnections).toBeGreaterThan(0);
+
+  expect(responseBody.usedConnections).toBeDefined();
+
+  expect(typeof responseBody.usedConnections).toBe("number");
+  expect(responseBody.usedConnections).toBeLessThanOrEqual(
+    responseBody.maxConnections,
+  );
+  expect(responseBody.usedConnections).toEqual(1);
 });
