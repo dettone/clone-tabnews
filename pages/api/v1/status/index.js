391b90c783eb57a8316a212e53c32f75daeb9345
dettone <dettonex25@gmail.com>
2025-10-09 18:21:47 -0300

    feat: create and use InternalServerError in api/v1/status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,23 +1,34 @@
 import database from "infra/database";
+import { InternalServerError } from "infra/erros";
 
 async function status(request, response) {
-  const databaseName = process.env.POSTGRES_DB;
-  const version = await database.query("SHOW server_version;");
+  try {
+    const databaseName = process.env.POSTGRES_DB;
+    const version = await database.query("SHOW server_version;");
 
-  const maxConnections = await database.query("SHOW max_connections");
-  const updateAt = new Date().toISOString();
+    const maxConnections = await database.query("SHOW max_connections");
+    const updateAt = new Date().toISOString();
 
-  const usedConnections = await database.query({
-    text: `SELECT count(*) AS used_connections FROM pg_stat_activity WHERE datname = $1;`,
-    values: [databaseName],
-  });
-  console.log(usedConnections.rows);
-  response.status(200).json({
-    updated_at: updateAt,
-    version: version.rows[0].server_version,
-    maxConnections: Number(maxConnections.rows[0].max_connections),
-    usedConnections: Number(usedConnections.rows[0].used_connections),
-  });
+    const usedConnections = await database.query({
+      text: `SELECT count(*) AS used_connections FROM pg_stat_activity WHERE datname = $1;`,
+      values: [databaseName],
+    });
+    console.log(usedConnections.rows);
+    response.status(200).json({
+      updated_at: updateAt,
+      version: version.rows[0].server_version,
+      maxConnections: Number(maxConnections.rows[0].max_connections),
+      usedConnections: Number(usedConnections.rows[0].used_connections),
+    });
+  } catch (error) {
+    const publicErrorObject = new InternalServerError({
+      cause: error,
+    });
+
+    console.log("\n Erro dentro do catch do controller:");
+    console.error(publicErrorObject);
+    response.status(500).json(publicErrorObject);
+  }
 }
 
 export default status;
